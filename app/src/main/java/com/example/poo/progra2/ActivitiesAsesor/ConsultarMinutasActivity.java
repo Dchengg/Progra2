7e2ff68808e4f24102edc4b2dfee9236a7eaeab9
Dchengg <diegocheng1973@gmail.com>
2018-05-23 22:49:56 -0600

    Acivities de Asesor casi completas
    Falta consultar Minuta y  crear la transición en el menu de asesor

---
+++ app/src/main/java/com/example/poo/progra2/ActivitiesAsesor/ConsultarMinutasActivity.java
@@ -0,0 +1,15 @@
+package com.example.poo.progra2.ActivitiesAsesor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.poo.progra2.R;
+
+public class ConsultarMinutasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_minutas);
+    }
+}
